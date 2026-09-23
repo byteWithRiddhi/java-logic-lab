77e611303de429d94ff07d2223cc614d048a1d43
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-23 22:12:20 +0530

    Add perfect number program

---
+++ 01-Basics/PerfectNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        // Find proper divisors
+        for (int i = 1; i <= n / 2; i++) {
+
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == n) {
+            System.out.println(n + " is a perfect number.");
+        } else {
+            System.out.println(n + " is not a perfect number.");
+        }
+
+        sc.close();
+    }
+}
