fc142c7d2d52a38f1ae90f732b532d6bf7e759d4
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-23 22:42:04 +0530

    Add character triangle pattern program

---
+++ 02-Pattern/CharacterTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CharacterTriangle {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        char ch = 'A';
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
