35b2b548cf20e4578aba810cc3c1cbe09b3cb0f8
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-23 22:41:30 +0530

    Add butterfly star pattern program

---
+++ 02-Pattern/ButterflyPattern.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class ButterflyPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= rows; i++) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (rows - i); j++) {
+                System.out.print("  ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = rows; i >= 1; i--) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (rows - i); j++) {
+                System.out.print("  ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
