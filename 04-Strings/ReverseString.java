f45de1b8436b9775b12a3df6fe2d446fe389360c
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-24 20:36:29 +0530

    Add program to reverse a string

---
+++ 04-Strings/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+
+        sc.close();
+    }
+}
