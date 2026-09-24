8edd58093d9fd4b2fbf95e33d133141702b70a04
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-24 20:55:19 +0530

    Add bubble sort algorithm in Java

--- 05-Sorting/BubbleSort.java
+++ 05-Sorting/BubbleSort.java
@@ -17,7 +17,6 @@ public class BubbleSort {
             arr[i] = sc.nextInt();
         }
 
-        // Bubble Sort Algorithm
         for (int i = 0; i < n - 1; i++) {
 
             for (int j = 0; j < n - i - 1; j++) {
