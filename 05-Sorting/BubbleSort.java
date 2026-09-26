a54ca83e7d37a22c4b3b693da5fd1dfff9d093a2
Riddhi Pawar <riddhipawar277@gmail.com>
2026-09-26 12:35:11 +0530

    Add bubble sort algorithm in Java
    Bubble Sort repeatedly compares adjacent elements and swaps them if they are in the wrong order.
    
    Compare adjacent elements.
    Swap them if the left element is greater.
    Repeat until the array is sorted.

--- 05-Sorting/BubbleSort.java
+++ 05-Sorting/BubbleSort.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 public class BubbleSort {
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
 
         System.out.println("Enter array elements:");
@@ -35,7 +33,6 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-
         sc.close();
     }
 }
